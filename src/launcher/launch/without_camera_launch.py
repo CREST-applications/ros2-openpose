ce9c38e0d9958b6d4badfd622f16dfae4b494de3
jme <jme.co.jp@gmail.com>
2024-09-09 20:00:52 +0900

    Update compose.yml and add without_camera_launch.py

---
+++ src/launcher/launch/without_camera_launch.py
@@ -0,0 +1,17 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            Node(
+                package="display",
+                executable="main",
+            ),
+            Node(
+                package="pose",
+                executable="main",
+            ),
+        ]
+    )
