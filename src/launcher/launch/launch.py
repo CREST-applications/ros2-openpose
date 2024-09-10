db75f52608f66dcf33254a330f51dae624f75486
jme <jme.co.jp@gmail.com>
2024-09-10 15:16:26 +0900

    camera_node

--- src/launcher/launch/launch.py
+++ src/launcher/launch/launch.py
@@ -5,21 +5,25 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription(
         [
+            # Node(
+            #     package="v4l2_camera",
+            #     executable="v4l2_camera_node",
+            # ),
+            # Node(
+            #     package="image_transport",
+            #     executable="republish",
+            #     arguments=["compressed"],
+            #     remappings=[("/in", "/image_raw"), ("/out", "/image_raw/compressed")],
+            # ),
+            # Node(
+            #     package="image_transport",
+            #     executable="republish",
+            #     arguments=["raw"],
+            #     remappings=[("/in", "/image_raw/compressed"), ("/out", "/camera")],
+            # ),
             Node(
-                package="v4l2_camera",
-                executable="v4l2_camera_node",
-            ),
-            Node(
-                package="image_transport",
-                executable="republish",
-                arguments=["raw"],
-                remappings=[("/in", "/image_raw"), ("/out", "/image_raw/compressed")],
-            ),
-            Node(
-                package="image_transport",
-                executable="republish",
-                arguments=["raw"],
-                remappings=[("/in", "/image_raw/compressed"), ("/out", "/camera")],
+                package="camera",
+                executable="main",
             ),
             Node(
                 package="display",
