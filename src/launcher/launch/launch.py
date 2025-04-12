654eddc84860b8bba36919c030d1fa8d7172050e
jmessk <jmerusty@gmail.com>
2025-04-12 15:09:04 +0900

    change pose submodule to normal directory

--- src/launcher/launch/launch.py
+++ src/launcher/launch/launch.py
@@ -4,30 +4,29 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription(
-        [            
-            Node(
-                package="v4l2_camera",
-                executable="v4l2_camera_node",
-            ),
+        [
             Node(
                 package="image_transport",
                 executable="republish",
                 arguments=["compressed"],
-                remappings=[("/in/compressed", "/image_raw/compressed")],
+                remappings=[
+                    ("/in/compressed", "/image_raw/compressed"),
+                    ("/out/compressed", "/proxy/compressed"),
+                ],
             ),
             Node(
                 package="pose",
                 executable="main",
-                remappings=[("/camera", "/out/compressed")],
+                remappings=[("/camera", "/proxy/compressed")],
             ),
             Node(
                 package="renderer",
                 executable="main",
-                remappings=[("/camera", "/out/compressed")],
+                remappings=[("/camera", "/proxy/compressed")],
             ),
             Node(
-                package="rqt_image_view",
-                executable="rqt_image_view",
+                package="display",
+                executable="main",
             ),
         ]
     )
